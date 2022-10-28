61645f7f7e7ebefe7008e42fdf11f6356e73142f
ajzambra <ajzambra@espol.edu.ec>
2022-10-27 20:29:17 -0500

    Creando base

---
+++ Seccion-B/Violacion DIP.java
@@ -0,0 +1,16 @@
+public class Compra {
+    private Pago pago;
+    private List articulos;
+
+    public Compra (Pago pago){
+        //inicializaciones
+    }
+    
+    public void agregarArticulos (Articulo articulo){
+        //Agrega un articulo a la compra
+    }
+    
+    public void removerArticulo(Articulo articulo){
+        //Remover un articulo a la compra
+    }
+}
