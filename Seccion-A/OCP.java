f397c84408580180d3f0774f5a54acf304b7b514
unknown <ricluper@espol.edu.ec>
2022-10-27 20:51:45 -0500

    Correccion OCP, SRP

--- Seccion-A/OCP.java
+++ Seccion-A/OCP.java
@@ -1,4 +1,61 @@
+//OCP
+//si se necesitan mas tipos de generar reportes se debe modificar la clase
 
-public class OCP {
+public class Reportes{
+	public void reporteEmpleados(List empleados, String tipo) {
+		if(Objects.equals(tipo, "PDF")){
+			reportePDF(empleados);
+		}
+		else if(Objects.equals(tipo, "WORD")) {
+			reporteWORD(empleados);
+		}
+		else {
+			reporteEXCEL(empleados);
+		}
+	}
+	
+	public void reportePDF(List empleados) {
+		//generar reporte en PDF
+	}
+	public void reporteWORD(List empleados) {
+		//generar reporte en WORD
+	}
+	public void reporteEXCEL(List empleados) {
+		//generar reporte en EXCEL
+	}
+}
 
+//MANERA CORRECTA
+//si necesitan otro tipo de reporte, solo se crea otra clase de ese tipo e implementar
+//la interfaz generadorreporte, ya no modificar la clase inicial
+
+public interface GeneradorReporte{
+	public void generarReporte(List empleados);
+}
+
+public class ReportePDF implements GeneradorReporte{
+	//atributos
+	public void generarReporte(List empleados) {
+		//generar reporte en PDF
+	}
+}
+
+public class ReporteWORD implements GeneradorReporte{
+	//atributos
+	public void generarReporte(List empleados) {
+		//generar reporte en WORD
+	}
+}
+
+public class ReporteEXCEL implements GeneradorReporte{
+	//atributos
+	public void generarReporte(List empleados) {
+		//generar reporte en EXCEL
+	}
+}
+
+public class Reportes{
+	public void reporteEmpleados(List empleados, GeneradorReporte generador) {
+		generador.generarReporte(empleados);
+	}
 }
--- Seccion-A/SRP.java
+++ Seccion-A/SRP.java
@@ -1,4 +1,48 @@
+//SRP
+//muchas responsabilidades y si cambia la manera de calcular valores, se debe modificar
+public class Empresa{	
+	public double PagoFinal(Empleado empleado){
+		double valor = this.valorAPagar(empleado);
+		double multas = this.multasADescontar(empleado)
+		return valor - multas;
+	}	
+	public double valorAPagar(Empleado empleado) {
+		double valor;
+		//calcular valor 
+		return valor;
+	}
+	public double multasADescontar(Empleado empleado) {
+		double multas;
+		//calcular multas
+		return multas;
+	}
+}
 
-public class SRP {
+//MANERA CORRECTA
+//si cambia la manera de calcular el valor o las multas, no afectara a la clase empresa
+//esta solo se encarga de entregar el valor final
 
+public class Empresa{
+	public double PagoFinal(Empleado empleado){
+		double valor = ValorTrabajo.valorAPagar(empleado);
+		double multas = MultasPorDescontar.multasADescontar(empleado)
+		return valor - multas;
+	}	
+}
+
+public class ValorTrabajo{
+	public static double valorAPagar(Empleado empleado) {
+		double valor;
+		//calcular valor 
+		return valor;
+	}
+
+}
+
+public class MultasPorDescontar{
+	public static double multasADescontar(Empleado empleado) {
+		double multas;
+		//calcular multas
+		return multas;
+	}
 }
